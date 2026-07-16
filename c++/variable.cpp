2692a57996ef9da5477b90518164fecb588f58e4
vineet yadav <vineet2020h@gmail.com>
2026-07-16 09:56:23 +0000

    c++

---
+++ c++/variable.cpp
@@ -0,0 +1,48 @@
+# include <iostream>
+using namespace std;
+
+// int main(){
+//     // // cout<<"hello world"<<endl;
+//     // int a=5;
+//     // cout<<"the age of vineet is "<<a<<endl;
+//     // float s=238.3;
+//     // cout<<"the score of vineet is "<<s<<endl;
+//     // // aise he double(bada decimal),char,string,long,long long, short etc..;
+
+
+
+
+//     // user se input lene 
+//     int a,b ;
+
+//     cout<<"enter the number"<<endl;
+//     cin>>a;
+//     cout<<"enter the number"<<endl;
+//     cin>>b;
+
+//     cout<<"a+b is "<<a+b<<endl;
+//     cout<<"a-b is "<<a-b<<endl;
+//     cout<<"a*b is "<<a*b<<endl;
+//     cout<<"a/b is "<<a/b<<endl;
+
+
+//     return 0;
+    
+// }
+
+
+//condition
+//if else
+// elseifelse
+//switch case
+
+
+//loop
+// while 
+// do while
+// for 
+
+
+// /function
+
+int(int a,int b)
